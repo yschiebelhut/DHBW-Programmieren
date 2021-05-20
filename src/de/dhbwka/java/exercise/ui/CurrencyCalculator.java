003c3408e13c3fd5615ae726b6e2e190d4db5ee5
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-05-20 20:36:27 +0200

    implemented functionality for CurrencyCalculator

--- src/de/dhbwka/java/exercise/ui/CurrencyCalculator.java
+++ src/de/dhbwka/java/exercise/ui/CurrencyCalculator.java
@@ -4,6 +4,9 @@
 package de.dhbwka.java.exercise.ui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,19 +16,26 @@ import javax.swing.JTextField;
  * @author Yannik Schiebelhut
  *
  */
-public class CurrencyCalculator extends JFrame {
+public class CurrencyCalculator extends JFrame implements ActionListener {
+
+	private final double CHANGE_RATE = 1.09;
+	
+	JButton etod = new JButton("EUR -> USD");
+	JButton dtoe = new JButton("USD -> EUR");
+	JButton cancel = new JButton("Cancel");
+	JTextField iofield = new JTextField("Please enter amount to convert!");
 
 	public CurrencyCalculator() {
 		this.setTitle("Currency Converter");
 		this.setLayout(new BorderLayout(5, 5));
 
-		JTextField iofield = new JTextField("Please enter amount to convert!");
 		iofield.requestFocus();
 		iofield.selectAll();
+		
+		etod.addActionListener(this);
+		dtoe.addActionListener(this);
+		cancel.addActionListener(this);
 
-		JButton etod = new JButton("EUR -> USD");
-		JButton dtoe = new JButton("USD -> EUR");
-		JButton cancel = new JButton("Cancel");
 
 		this.add(iofield, BorderLayout.NORTH);
 		this.add(etod, BorderLayout.WEST);
@@ -41,6 +51,36 @@ public class CurrencyCalculator extends JFrame {
 	 */
 	public static void main(String[] args) {
 		new CurrencyCalculator();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton source = (JButton) e.getSource();
+		if(source == this.cancel) {
+			System.exit(0);
+		} else {
+			try {
+				double amount = Double.parseDouble(this.iofield.getText());
+				
+				if (source == this.etod) {
+					amount = amount * CHANGE_RATE;
+				} else {
+					amount = amount / CHANGE_RATE;
+				}
+				this.iofield.setText(Double.toString(round(amount)));
+			} catch (NumberFormatException ne) {
+				this.iofield.setText("invalid input format");
+			} finally {
+				iofield.requestFocus();
+				iofield.selectAll();
+			}
+		}
+	}
+	
+	public double round(double d) {
+		BigDecimal b = new BigDecimal(d);
+		BigDecimal round = b.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+		return round.doubleValue();
 	}
 
 }
