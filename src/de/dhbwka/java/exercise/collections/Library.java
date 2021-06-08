7f96ae94fe2625f53d16349a868d7c4c471175bd
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-06-08 11:10:05 +0200

    did some beautifying

--- src/de/dhbwka/java/exercise/collections/Library.java
+++ src/de/dhbwka/java/exercise/collections/Library.java
@@ -2,28 +2,19 @@ package de.dhbwka.java.exercise.collections;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class Library extends JFrame {
 
-	private JTextField title = new JTextField();
-	private JTextField author = new JTextField();
-	private JTextField year = new JTextField();
-	private JTextField publisher = new JTextField();
-
-	private JButton save = new JButton("Save entry");
-	private JButton titleBtn = new JButton("Title");
-	private JButton authorBtn = new JButton("Author");
-	private JButton yearBtn = new JButton("Year");
-	private JButton publisherBtn = new JButton("Publisher");
+	private final JTextField title = new JTextField();
+	private final JTextField author = new JTextField();
+	private final JTextField year = new JTextField();
+	private final JTextField publisher = new JTextField();
 
 	private final File libraryFile = new File("src/de/dhbwka/java/exercise/collections/library.csv");
-	private ArrayList<Book> library = new ArrayList<>();
+	private final ArrayList<Book> library = new ArrayList<>();
 
 	public Library() {
 		this.setTitle("Library");
@@ -43,10 +34,11 @@ public class Library extends JFrame {
 		data.add(publisher);
 		wrapper.add(data);
 
+		JButton save = new JButton("Save entry");
 		wrapper.add(save);
 		save.addActionListener(e -> {
 			try {
-				Book b = new Book(title.getText(),author.getText(),Integer.parseInt(year.getText()),publisher.getText());
+				Book b = new Book(title.getText(), author.getText(), Integer.parseInt(year.getText()), publisher.getText());
 				if (!library.contains(b)) {
 					library.add(b);
 					try (PrintWriter p = new PrintWriter(new FileWriter(libraryFile, true))) {
@@ -61,54 +53,45 @@ public class Library extends JFrame {
 
 		JPanel order = new JPanel();
 		order.add(new JLabel("Ordered output:"));
+		JButton titleBtn = new JButton("Title");
 		order.add(titleBtn);
 		titleBtn.addActionListener(e -> {
-			Comparator<Book> c = new Comparator<Book>() {
-				@Override
-				public int compare(Book o1, Book o2) {
-					return o1.getTitle().compareToIgnoreCase(o2.getTitle());
-				}
-			};
+//			Comparator<Book> c = new Comparator<Book>() {
+//				@Override
+//				public int compare(Book o1, Book o2) {
+//					return o1.getTitle().compareToIgnoreCase(o2.getTitle());
+//				}
+//			};
+			Comparator<Book> c = (o1, o2) -> o1.getTitle().compareToIgnoreCase(o2.getTitle());
 			System.out.println(library);
 			library.sort(c);
 			System.out.println(library);
 			displayLibrary();
 		});
+		JButton authorBtn = new JButton("Author");
 		order.add(authorBtn);
 		authorBtn.addActionListener(e -> {
-			Comparator<Book> c = new Comparator<Book>() {
-				@Override
-				public int compare(Book o1, Book o2) {
-					return o1.getAuthor().compareToIgnoreCase(o2.getAuthor());
-				}
-			};
+			Comparator<Book> c = (o1, o2) -> o1.getAuthor().compareToIgnoreCase(o2.getAuthor());
 			System.out.println(library);
 			library.sort(c);
 			System.out.println(library);
 			displayLibrary();
 		});
 
+		JButton yearBtn = new JButton("Year");
 		order.add(yearBtn);
 		yearBtn.addActionListener(e -> {
-			Comparator<Book> c = new Comparator<Book>() {
-				@Override
-				public int compare(Book o1, Book o2) {
-					return o1.getYear()-o2.getYear();
-				}
-			};
+//			Comparator<Book> c = (o1, o2) -> o1.getYear() - o2.getYear();
 			System.out.println(library);
-			library.sort(c);
+//			library.sort(c);
+			library.sort(Comparator.comparingInt(Book::getYear));
 			System.out.println(library);
 			displayLibrary();
 		});
+		JButton publisherBtn = new JButton("Publisher");
 		order.add(publisherBtn);
 		publisherBtn.addActionListener(e -> {
-			Comparator<Book> c = new Comparator<Book>() {
-				@Override
-				public int compare(Book o1, Book o2) {
-					return o1.getPublisher().compareToIgnoreCase(o2.getPublisher());
-				}
-			};
+			Comparator<Book> c = (o1, o2) -> o1.getPublisher().compareToIgnoreCase(o2.getPublisher());
 			System.out.println(library);
 			library.sort(c);
 			System.out.println(library);
@@ -131,8 +114,6 @@ public class Library extends JFrame {
 				String[] bookData = curLine.split(";");
 				library.add(new Book(bookData[0], bookData[1], Integer.parseInt(bookData[2]), bookData[3]));
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
