d2556968a3f24ed7c6979d1699a1ec4a7333ae8f
Yannik Schiebelhut <yannik.schiebelhut@gmail.com>
2021-06-08 12:03:08 +0200

    added implementation for BinaryTree

---
+++ src/de/dhbwka/java/exercise/collections/BinaryTree.java
@@ -0,0 +1,68 @@
+package de.dhbwka.java.exercise.collections;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class BinaryTree<T extends Comparable<T>> {
+	private T value;
+	private BinaryTree<T> left, right;
+
+	public BinaryTree() {
+	}
+
+	public boolean isEmpty() {
+		return this.value == null;
+	}
+
+	public boolean add(T x) {
+		if (this.isEmpty()) {
+			this.value = x;
+			return true;
+		}
+		if (x.compareTo(this.value) < 0) {
+			if (left == null) {
+				left = new BinaryTree<T>();
+			}
+			this.left.add(x);
+			return true;
+		}
+		if (x.compareTo(this.value) > 0) {
+			if (right == null) {
+				right = new BinaryTree<T>();
+			}
+			this.right.add(x);
+			return true;
+		}
+		return false;
+	}
+
+	private T getValue() {
+		return this.value;
+	}
+
+	public List<T> traverse() {
+		List<T> out = new LinkedList<>();
+		if(this.left != null) {
+			out.addAll(left.traverse());
+		}
+		out.add(this.getValue());
+		if(this.right != null) {
+			out.addAll(right.traverse());
+		}
+		return out;
+	}
+
+	public static void main(String[] args) {
+		BinaryTree<Integer> tree = new BinaryTree<>();
+		{
+			Random r = new Random();
+			for (int i = 0; i<10; i++) {
+				int rand = r.nextInt(100);
+				System.out.println(rand);
+				tree.add(rand);
+			}
+		}
+		System.out.println(tree.traverse());
+	}
+}
